2817afe61b6414674e91f6fe02ca4fa2b476b611
Satyaprakash078 <satyaprakashjena235@gmail.com>
2024-12-18 14:08:52 +0530

    Update backend with MongoDB Atlas connection and other changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Port=process.env.PORT || 5000;
 
 //middleware setup
 app.use(express.json({limit:"25mb"}));
-app.use(express.urlencoded({limit:"25mb"}));
+//app.use(express.urlencoded({limit:"25mb"}));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
